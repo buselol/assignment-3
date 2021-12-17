24c556f7ff5802308203ac6bed6a34da2487f32c
Buse <Buse@DESKTOP-7EVT6O5>
2021-12-17 18:38:42 +0100

    Proje dosyası ekle.

---
+++ of/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+
+
+
+
+
+
+
+    {
+
+        Console.WriteLine("Enter the numbr ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        
+        
+        
+        
+        
+        
+        bool[] prime = new bool[n + 1]; 
+        for (int i = 2; i <= n; i++) prime[i] = true;
+
+        
+        
+        
+        
+        
+        int limit = (int)Math.Ceiling(Math.Sqrt(n));
+
+        for (int i = 2; i <= limit; i++)
+            if (prime[i])
+                for (int j = i * i; j <= n; j += i)
+                    prime[j] = false;
+
+        Console.WriteLine("Prime Numbers ", n);
+
+        for (int i = 0; i <= n; i++)
+            if (prime[i])
+                Console.WriteLine(i);
+    }
+
+}
